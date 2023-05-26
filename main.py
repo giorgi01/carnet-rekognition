55dccb51f25601906eceb68bef9708ad8a24d570
Giorgi Tamarashvili <giorgi.tamarashvili.1@btu.edu.ge>
2023-05-26 18:59:25 +0400

    add zip support

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import os
+import zipfile
 import boto3
 import argparse
 from os import getenv
 from my_auto_parser import parse_and_save_to
 
 
-def upload_files_to_s3(bucket_name, local_directory):
+def upload_files_to_s3(bucket_name, file_path):
     s3_client = boto3.client('s3',
                              aws_access_key_id=getenv("aws_access_key_id"),
                              aws_secret_access_key=getenv("aws_secret_access_key"),
                              aws_session_token=getenv("aws_session_token"),
                              region_name=getenv("aws_region_name"))
 
-    for root, dirs, files in os.walk(local_directory):
-        for file in files:
-            local_path = os.path.join(root, file)
-            relative_path = os.path.relpath(local_path, local_directory)
+    if file_path.endswith('.zip'):
+        with zipfile.ZipFile(file_path, 'r') as zip_ref:
+            for file in zip_ref.namelist():
+                print(f"Uploading {file} to s3://{bucket_name}/{file}")
+                s3_client.upload_fileobj(zip_ref.open(file), bucket_name, file)
+    else:
+        for root, dirs, files in os.walk(file_path):
+            for file in files:
+                local_path = os.path.join(root, file)
+                relative_path = os.path.relpath(local_path, file_path)
 
-            print(f"Uploading {local_path} to s3://{bucket_name}/{relative_path}")
-            s3_client.upload_file(local_path, bucket_name, relative_path)
+                print(f"Uploading {local_path} to s3://{bucket_name}/{relative_path}")
+                s3_client.upload_file(local_path, bucket_name, relative_path)
 
 
 if __name__ == '__main__':
